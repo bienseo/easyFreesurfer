cdaa5e485de0b07a2ee9e1ea9158d29bbe8090ee
kangik <kangik@neuroimage.snu.ac.kr>
2014-07-07 15:36:41 +0900

    first commit

---
+++ freesurfer.py
@@ -0,0 +1,58 @@
+#!/Users/admin/anaconda/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+import os
+import argparse
+import textwrap
+
+
+def findT1Dir(dir):
+    #Regrex for T1
+    t1 = re.compile(r'tfl|[^s]t1|t1',re.IGNORECASE)
+
+    try:
+        for root,dirs,files in os.walk(dir):
+            if t1.search(os.path.basename(root)):
+                print root
+                t1Directory = root
+                t1Dcm = os.listdir(root)[0]
+                t1DcmAddress = os.path.join(t1Directory,t1Dcm)
+    except:
+        sys.exit('Arrange T1 dicoms in the T1 directory')
+
+    return t1Directory,t1DcmAddress
+
+def main(args):
+    subjAddress = args.directory
+    #print subjAddress
+    t1Directory,t1DcmAddress = findT1Dir(subjAddress)
+
+    outDirectory = os.path.dirname(t1Directory)
+
+    os.environ["FREESURFER_HOME"] = '/Applications/freesurfer'
+    os.environ["SUBJECTS_DIR"] = '{0}'.format(outDirectory)
+
+    freesurferScreenOutput = os.popen('recon-all -all -i {t1DcmAddress} -subjid freesurfer'.format(
+            t1DcmAddress=t1DcmAddress)).read()
+
+    print t1Directory,t1DcmAddress,freesurferScreenOutput
+
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+            description = textwrap.dedent('''\
+                    {codeName} : Runs freesurfer recon-all -all
+                    ====================
+                        eg) {codeName}
+                        eg) {codeName} --dir /Users/kevin/NOR04_CKI
+                    '''.format(codeName=os.path.basename(__file__))))
+
+            #epilog="By Kevin, 26th May 2014")
+    parser.add_argument('-dir','--directory',help='Data directory location, default = pwd',default=os.getcwd())
+    args = parser.parse_args()
+
+    main(args)
+
